cf55cdf0f8e504836e727fb8acf6bc47cb644570
ZAKARIA666 <121687508+ZAKARIA666@users.noreply.github.com>
2023-03-11 20:35:51 +0100

    first commit

---
+++ myapp/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import H from './cmponents/H';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <H/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myapp/src/cmponents/H.js
@@ -0,0 +1,90 @@
+import React from 'react'
+import './H.css';
+
+function H() {
+    return (
+
+
+      <div>  
+
+            <div className='box1' >
+                <h1 className='bb'> FSM RoboTech </h1>
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+
+<div className='deux'>
+  <h3>  About FSM RoboTech  </h3>   
+  <h4>  Membres : </h4>
+   <div className='image'>
+  <img src='https://scontent.ftun6-1.fna.fbcdn.net/v/t39.30808-6/334564303_192083383442746_4423987066863384839_n.jpg?stp=cp6_dst-jpg&_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=n2Q52KPl5BYAX9fXTyR&_nc_ht=scontent.ftun6-1.fna&oh=00_AfCNYOTcqzxazoWuGmXCpf9YmOPtj-DOVqPcNMyIR8i5XA&oe=64106F6A'></img>
+  <img src='https://scontent.ftun6-1.fna.fbcdn.net/v/t39.30808-6/334073387_1850284191998808_4403091063779818563_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=CRuC-pdg2ZkAX9DF4k2&_nc_ht=scontent.ftun6-1.fna&oh=00_AfALwmr-m738TIWUV5hlunNm2lqYcJYp0EKsL-S9lqDK-Q&oe=640FD37B'></img>
+  <img src='https://scontent.ftun6-1.fna.fbcdn.net/v/t39.30808-6/334185745_3600209816876067_3614906873474648778_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=730e14&_nc_ohc=rwG7ADSB93IAX-st_fa&_nc_ht=scontent.ftun6-1.fna&oh=00_AfDKTgmaPArsvwZgO3xfwLdTo6iyd2MISsne1CDOMlZDLA&oe=64104AE3'></img>
+  
+ </div>
+   
+ 
+
+  </div>
+  <div className='act'>  
+  <h5> Activities :</h5>
+  <ul>
+  <li> design and build robots</li>
+  <li> practical advices about challenges</li>
+  <li> participating in many differents events</li>
+  </ul>
+  </div>
+
+            <div className='boox' > 
+            Espace de Membres
+            
+            </div>
+            <br></br>
+
+
+            <br></br>
+<div  className='react'>
+<div           className='cc'> Log in</div></div>
+            <div className='box2'></div>
+            <form>
+                <div className='mb-3 ' >
+                    <label for="exampleInputEmail1" className="form-label">Email address </label>
+                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" />
+                    <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+                </div>
+                <div className='mb-3'>
+                    <label for="exampleInputPassword1" className="form-label">Password</label>
+                    <input type="password" className="form-control" id="exampleInputPassword1" />
+                </div>
+                <div class="mb-3 form-check">
+                    <input type="checkbox" className="form-check-input" id="exampleCheck1" />
+                    <label className="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <div className='ll' >  
+                <button type="submit" className="btn btn-primary">Submit</button>
+                
+                </div>
+            </form>
+
+<br/>
+
+<br></br>
+
+
+
+
+
+            <div className='contact' >  
+            <p> contact :</p>
+            <a href='https://www.instagram.com/fsm.robotech/'> instagram </a>  <br></br>
+            <a href='      https://www.facebook.com/FSMROBOTECH/?locale=fr_FR'>facebook</a> 
+  
+        </div>
+        </div>
+    ) 
+}
+
+export default H
+
+
